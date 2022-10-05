5618fbd21a7bff7d7e5c5644a13ea5ba2543a7b5
gabe19p <g19purselley@gmail.com>
2022-10-05 20:55:41 +0900

    steps 1-3 f/ console app

---
+++ gryffindor_WhatABook.py
@@ -0,0 +1,33 @@
+
+# Title: gryffindor_WhatABook.py
+# Author: Danial Purselley
+# Author: Ferdinand Detres
+# Date: 5 Oct 2022
+# Description: connecting to mongoDB
+# with python
+
+# import statement
+from ast import Or
+from pymongo import MongoClient
+
+# connecting to the database
+client = MongoClient(
+    "mongodb+srv://web335_user:s3cret@buwebdev-cluster-1.078ar.mongodb.net/web335DBretryWrites=true&w=majority")
+
+# database variable
+db = client['web335DB']
+
+# display the books collection in legible format
+print("Here is our list of books: ")
+for book in db.books.find({}, {'_id': 0, 'bookId': 0}):
+    print(book)
+
+# print list of books based on user's selected genre
+user_Choice = input("Please enter a genre from above:")
+if user_Choice == 'Educational' or user_Choice == 'horror' or user_Choice == 'drama':
+    for book in db.books.find({'genre': user_Choice}, {'title': 1, 'genre': 1}):
+        print(book)
+else:
+    print("The genre you provided is invalid")
+
+# display a customers wishlist
