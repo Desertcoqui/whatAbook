ba8423f09b8b9cf9855045b48f03bc637dd1cc31
gabe19p <g19purselley@gmail.com>
2022-10-05 20:55:25 +0900

    finished js queries

--- gryffindor-whatAbook.js
+++ gryffindor-whatAbook.js
@@ -98,7 +98,6 @@ johnMacro = {
     ],
   },
 };
-
 rebeccaMacro = {
   firstName: "Rebecca",
   lastName: "Macro",
@@ -213,3 +212,36 @@ db.books.aggregate([{ $sort: { author: 1 } }]);
 
 //display books by bookId
 db.books.aggregate([{ $sort: { bookId: 1 } }]);
+
+//display wishlist by customer id
+db.customers.find({ customerId: "01" }, { wishlistItems: 1 });
+
+//add book to customers wishlistItems
+db.customers.updateOne(
+  { customerId: "01" },
+  {
+    $push: {
+      "wishlistItems.books": {
+        title: "Lord of the Rings",
+        genre: "Fantasy",
+        author: "J. R. R. Tolkien",
+        bookId: "6969",
+      },
+    },
+  }
+);
+
+//remove a book from a customer's wishlistItems
+db.customers.updateOne(
+  { customerId: "01" },
+  {
+    $pull: {
+      "wishlistItems.books": {
+        title: "Lord of the Rings",
+        genre: "Fantasy",
+        author: "J. R. R. Tolkien",
+        bookId: "6969",
+      },
+    },
+  }
+);
